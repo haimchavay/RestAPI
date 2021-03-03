c988cb447aa27d1a115b029f7220dcb48a5496a1
haim chavay <haimchavay2@gmail.com>
2021-03-03 14:21:54 +0200

    add another project | signalR

---
+++ SignalRChat/Controllers/BroadcastController .cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRChat.Hubs;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BroadcastController : ControllerBase
+    {
+        private readonly IHubContext<ChatHub> _hub;
+
+        public BroadcastController(IHubContext<ChatHub> hub)
+        {
+            _hub = hub;
+        }
+
+        [HttpGet]
+        public async Task Get(string user, string message)
+        {
+            await _hub.Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
